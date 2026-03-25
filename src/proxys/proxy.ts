e4612270bd18c794104ed7f96fe29626d020b6c4
xsalles <xs.salles@gmail.com>
2026-03-25 14:06:35 -0300

    refactored(): changed the name of my protector, next recommended change to proxy instead of middleware

--- src/proxys/proxy.ts
+++ src/proxys/proxy.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 const PROTECTED_ROUTES = ["/dashboard", "/advogado"];
 const AUTH_ROUTES = ["/login", "/onBoarding"];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const accessToken = request.cookies.get("access_token")?.value;
